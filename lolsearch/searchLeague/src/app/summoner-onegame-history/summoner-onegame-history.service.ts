e0faf7637823195f235bce2aa69df3ef3ed503fd
unknown <ykkim6@hotmail.com>
2018-08-21 22:57:53 -0400

    second

--- lolsearch/searchLeague/src/app/summoner-onegame-history/summoner-onegame-history.service.ts
+++ lolsearch/searchLeague/src/app/summoner-onegame-history/summoner-onegame-history.service.ts
@@ -25,11 +25,7 @@ export class SummonerOnegameHistoryService {
 
   setURL(gameid: string)
   {
-<<<<<<< HEAD
 	 this.apiurl = "/api/lol/match/v3/matches/" + gameid + "?api_key=RGAPI-cf0256aa-b524-4a91-9134-3d3404565252";
-=======
-	 this.apiurl = "/api/lol/match/v3/matches/" + gameid + "?api_key=RGAPI-e25b43b3-57a5-4b51-a6e3-2fddca5f7bd9";
->>>>>>> 5c7e29a9b614a085a2485b874a41a1db953dd689
   }
   
   getdata(match: Match): Observable<Player[]>
