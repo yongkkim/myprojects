9d39ab129cacc7b077c429852bafaf5929ed6b71
unknown <ykkim6@hotmail.com>
2018-08-21 22:54:40 -0400

    second

--- lolsearch/searchLeague/src/app/summoner-history/champs.ts
+++ lolsearch/searchLeague/src/app/summoner-history/champs.ts
@@ -1,9 +1,6 @@
 export interface Champs{
 	key: string;
-<<<<<<< HEAD
 	name: string;
-=======
->>>>>>> 5c7e29a9b614a085a2485b874a41a1db953dd689
 	image: Image;
 }
 class Image{
--- lolsearch/searchLeague/src/app/summoner/lolinterface.ts
+++ lolsearch/searchLeague/src/app/summoner/lolinterface.ts
@@ -5,8 +5,5 @@ export interface LOLUserData {
   revisionDate: number;
   id: number;
   accountId: number;
-<<<<<<< HEAD
   profileimg: string;
-=======
->>>>>>> 5c7e29a9b614a085a2485b874a41a1db953dd689
 }
