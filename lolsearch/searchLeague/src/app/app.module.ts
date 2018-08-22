bda86a7e6b899bfbe22946af0d4179ec22057097
unknown <ykkim6@hotmail.com>
2018-08-21 23:00:59 -0400

    second

--- lolsearch/searchLeague/src/app/app.module.ts
+++ lolsearch/searchLeague/src/app/app.module.ts
@@ -5,22 +5,15 @@ import { AppComponent } from './app.component';
 import { SummonerComponent } from './summoner/summoner.component';
 import { SummonerHistoryComponent } from './summoner-history/summoner-history.component';
 import { SummonerOnegameHistoryComponent } from './summoner-onegame-history/summoner-onegame-history.component';
-<<<<<<< HEAD
 import { ChampionStatsComponent } from './champion-stats/champion-stats.component';
-=======
->>>>>>> 5c7e29a9b614a085a2485b874a41a1db953dd689
 
 @NgModule({
   declarations: [
     AppComponent,
     SummonerComponent,
     SummonerHistoryComponent,
-<<<<<<< HEAD
     SummonerOnegameHistoryComponent,
     ChampionStatsComponent
-=======
-    SummonerOnegameHistoryComponent
->>>>>>> 5c7e29a9b614a085a2485b874a41a1db953dd689
   ],
   imports: [
     BrowserModule,
